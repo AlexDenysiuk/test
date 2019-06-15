a6a29ad87fce865c7d0518ee973408933ee34d5d
Alex <al.denysiuk@yandex.ru>
2019-06-15 10:57:16 +0300

    Первый комит

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+//
+// console.log(4/0);
+//
+// let work = ['tock.png','dick.jpg','dron.bmp'];
+// console.log(work[2]);
+//
+// alert("HEloo World!");
+
+let incr = 10,
+    decr = 10;
